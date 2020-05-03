e4c22cdaf03d823f1a7271e5a6a937a31bd2ea6a
an-apluss <boscotec.inc@gmail.com>
2020-05-04 00:32:57 +0100

    Updates

---
+++ precache-manifest.bc65ba97b0cbae283156ffe9f9012914.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e857bf5a0e940c0ed75d663f91ff2c64",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "78d3bee32516eae1d237",
+    "url": "/robofriends/static/css/2.c753365a.chunk.css"
+  },
+  {
+    "revision": "78e2856f669796822138",
+    "url": "/robofriends/static/css/main.a930f43e.chunk.css"
+  },
+  {
+    "revision": "78d3bee32516eae1d237",
+    "url": "/robofriends/static/js/2.f2bd4e9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f2bd4e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e2856f669796822138",
+    "url": "/robofriends/static/js/main.e8ac8875.chunk.js"
+  },
+  {
+    "revision": "5048e69972ec4d5ebeb1",
+    "url": "/robofriends/static/js/runtime-main.9e81d2bc.js"
+  },
+  {
+    "revision": "aee1bc0455b421464a71120840033fc5",
+    "url": "/robofriends/static/media/Pitstop-4wGB.aee1bc04.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
