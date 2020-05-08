e8ffe7d97262562928370b3d180f0face4897b3d
an-apluss <boscotec.inc@gmail.com>
2020-05-08 20:47:58 +0100

    activate service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import reduxThunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './container/App';
@@ -10,9 +10,9 @@ import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(rootReducer, applyMiddleware(reduxThunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(reduxThunkMiddleware));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -26,4 +26,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
