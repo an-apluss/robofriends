1a239c6ebe65b83232a618f442ee9dd3a8b5301b
an-apluss <boscotec.inc@gmail.com>
2020-05-08 22:00:44 +0100

    improve the performance of the app based on lighthouse suggestions

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
   const { id, name, email } = props;
   return (
     <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
-      <img className="br-100 bg-lightest-blue" src={`https://robohash.org/${id}?200x200`} alt='robot' width="70%" />
+      <img className="br-100 bg-lightest-blue" src={`https://robohash.org/${id}?size=200x200`} alt='robot' width="70%" />
       <div>
         <h3>{name}</h3>
         <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({searchField, searchChange}) => {
   return (
     <div className="pa2">
       <input 
+        aria-label="Search Robots"
         className="pa3 ba b--green bg-lightest-blue shadow-5 br2"
         type="text"
         placeholder='Search Robots'
